84b128c89dd27fb1b06c64f345d23c69dc1fbee8
Praveen <praveenpvg.71@gmail.com>
2023-12-17 19:02:29 +0530

    change the widths and heights of page content and title to be relative to the screen size

--- src/components/ui/PageTitle.tsx
+++ src/components/ui/PageTitle.tsx
@@ -7,8 +7,10 @@ type Props = {
 export function PageTitle({ children }: Props) {
   return (
     <>
-      <h1 className="mt-4 flex items-center justify-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-200">
-        {"~ "}{children}{" ~"}
+      <h1 className="flex items-center justify-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-gray-200">
+        {"~ "}
+        {children}
+        {" ~"}
       </h1>
     </>
   );

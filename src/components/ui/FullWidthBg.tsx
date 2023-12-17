6b36fe0a8f2a094a5cce7a79dbf5b9b370854947
Praveen <praveenpvg.71@gmail.com>
2023-12-17 19:05:01 +0530

    modify the background component to take space relative to the size of the screen

--- src/components/ui/FullWidthBg.tsx
+++ src/components/ui/FullWidthBg.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { cn } from "../../lib/utils";
 
 type Props = {
   children?: React.ReactNode;
@@ -9,9 +10,10 @@ export function FullWidthBg({ children, className }: Props) {
   return (
     <>
       <div
-        className={
-          " rounded bg-gray-50 dark:bg-gray-800 p-1 min-h-fit" + " " + className
-        }
+        className={cn(
+          "rounded bg-gray-50 dark:bg-gray-800 p-1 min-h-fit",
+          className
+        )}
       >
         {children}
       </div>

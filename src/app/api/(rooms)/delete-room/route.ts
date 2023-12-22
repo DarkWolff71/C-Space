1d9137bce310aa9566f7c71d2bc6018594c2906a
Praveen <praveenpvg.71@gmail.com>
2023-12-23 02:02:28 +0530

    add route for deleting a room

---
+++ src/app/api/(rooms)/delete-room/route.ts
@@ -0,0 +1,46 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../../(authentication)/auth/[...nextauth]/options";
+import { getPrismaClient } from "@/lib/prisma";
+
+let prisma = getPrismaClient();
+
+export async function DELETE(req: NextRequest) {
+  let roomNameQueryParam = req.nextUrl.searchParams.get("roomName");
+
+  let session = await getServerSession(authOptions);
+  if (!(roomNameQueryParam && session && session.user.email)) {
+    return NextResponse.json({ error: "Invalid request." }, { status: 400 });
+  }
+
+  let dbResult = await prisma.room.findUnique({
+    where: {
+      name: roomNameQueryParam,
+    },
+    select: {
+      _count: {
+        select: {
+          owners: true,
+        },
+      },
+      owners: {
+        where: {
+          email: session.user.email,
+        },
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+  if (!dbResult || dbResult?._count.owners > 1 || dbResult.owners.length != 1) {
+    return NextResponse.json({ error: "Invalid request." }, { status: 400 });
+  }
+
+  await prisma.room.delete({
+    where: {
+      name: roomNameQueryParam,
+    },
+  });
+  return NextResponse.json({ message: "Succedfully deleted the room." });
+}

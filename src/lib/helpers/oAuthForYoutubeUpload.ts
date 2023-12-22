b44895750504cc2514f8d159355d636f52c69cc3
Praveen <praveenpvg.71@gmail.com>
2023-12-22 23:05:45 +0530

    add role enum

---
+++ src/lib/helpers/oAuthForYoutubeUpload.ts
@@ -0,0 +1,14 @@
+import { google } from "googleapis";
+
+export const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "http://localhost:3000/api/test-upload-js"
+);
+const scopes =
+  "https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/userinfo.profile";
+
+export const oAuthYoutubeUrl = oAuth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: scopes,
+});

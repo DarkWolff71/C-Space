468e0375bd62911d8e3cdb42250cc3e20186f552
Praveen <praveenpvg.71@gmail.com>
2023-12-23 02:02:52 +0530

    add route for creating a room

---
+++ src/app/api/(rooms)/create-room/route.ts
@@ -0,0 +1,44 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../(authentication)/auth/[...nextauth]/options";
+import { NextRequest, NextResponse } from "next/server";
+import { createRoomRequestValidator } from "@/validators/roomsValidators";
+import { getPrismaClient } from "@/lib/prisma";
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  let prisma = getPrismaClient();
+
+  let parsedRequest = createRoomRequestValidator.safeParse(await req.json());
+  if (!parsedRequest.success) {
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+  }
+  let reqBody = parsedRequest.data;
+
+  let dbResult = await prisma.room.findFirst({
+    where: { name: reqBody.roomName },
+  });
+
+  if (dbResult) {
+    return NextResponse.json({
+      alreadyExists: "The room with the given name already exists",
+    });
+  }
+  let session = await getServerSession(authOptions);
+  if (session?.user?.email) {
+    await prisma.room.create({
+      data: {
+        name: reqBody.roomName,
+        owners: {
+          connect: {
+            email: session.user.email,
+          },
+        },
+      },
+    });
+  } else {
+    return NextResponse.json(
+      { errorMessage: "Session does not contain email address" },
+      { status: 500 }
+    );
+  }
+  return res;
+}

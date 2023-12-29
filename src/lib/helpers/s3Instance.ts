6d29365209db20cccad1ff84ac668c1b2ed592b4
Praveen <praveenpvg.71@gmail.com>
2023-12-29 22:56:31 +0530

    add singleton function for getting s3 client

--- src/lib/helpers/s3Instance.ts
+++ src/lib/helpers/s3Instance.ts
@@ -1,15 +1,15 @@
-import S3 from "aws-sdk/clients/s3";
+import { S3 } from "@aws-sdk/client-s3";
 
 let s3Instance: S3 | undefined;
 
 export function getS3Instance(): S3 {
   if (!s3Instance) {
     s3Instance = new S3({
-      apiVersion: "2006-03-01",
-      accessKeyId: process.env.AWS_ACCESS_KEY,
-      secretAccessKey: process.env.AWS_SECRET_KEY,
-      region: process.env.AWS_REGION,
-      signatureVersion: "v4",
+      region: process.env.AWS_REGION!,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_KEY!,
+      },
     });
   }
   return s3Instance;

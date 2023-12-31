1cfc302edc347e34536e444f655f56fe3ae26964
Praveen <praveenpvg.71@gmail.com>
2023-12-31 23:31:16 +0530

    update recoil  atoms

--- src/recoil-store/atoms/members.ts
+++ src/recoil-store/atoms/members.ts
@@ -1,5 +1,11 @@
+import { Role } from "@/lib/constants/roles";
 import { atom } from "recoil";
 export const ownersInCurrentRoom = atom<number | null>({
   key: "ownersInCurrentRoom",
   default: null,
+});
+
+export const roleInCurrentRoom = atom<Role>({
+  key: "roleInCurrentRoom",
+  default: Role.EDITOR,
 });
--- src/recoil-store/atoms/upload-video.ts
+++ src/recoil-store/atoms/upload-video.ts
@@ -5,7 +5,42 @@ export const categoryIdState = atom({
   default: "",
 });
 
-export const privacyStatusState = atom({
+export const privacyStatusState = atom<"private" | "unlisted" | "public">({
   key: "privacyStatus",
   default: "private",
+});
+
+export const videoDescription = atom({
+  key: "videoDescription",
+  default: "",
+});
+
+export const videoTitle = atom({
+  key: "videoTitle",
+  default: "",
+});
+
+export const videoTags = atom<string[]>({
+  key: "videoTags",
+  default: [],
+});
+
+export const videoId = atom<string | null>({
+  key: "videoId",
+  default: null,
+});
+
+export const videoFile = atom<File | null>({
+  key: "videoFile",
+  default: null,
+});
+
+export const isVideoFileChanged = atom<boolean>({
+  key: "isVideoFileChanged",
+  default: false,
+});
+
+export const videoUploadPercentage = atom<number>({
+  key: "videoUploadPercentage",
+  default: 0,
 });

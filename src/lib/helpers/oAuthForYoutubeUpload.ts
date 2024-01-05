cdedfa1d89e90017e5be22bafa66f9c0444b3702
Praveen <praveenpvg.71@gmail.com>
2024-01-05 23:42:28 +0530

    modify youtube oauth helper

--- src/lib/helpers/oAuthForYoutubeUpload.ts
+++ src/lib/helpers/oAuthForYoutubeUpload.ts
@@ -3,7 +3,7 @@ import { google } from "googleapis";
 export const oAuth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/api/test-upload-js"
+  "http://localhost:3000/api/yt-oauth-redirect"
 );
 const scopes =
   "https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/userinfo.profile";

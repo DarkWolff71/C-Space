23eda6622e30f61403eab950fec3714d7beb6295
Praveen <praveenpvg.71@gmail.com>
2024-01-07 00:39:32 +0530

    final get room members route

--- src/app/api/(members)/get-room-members/route.ts
+++ src/app/api/(members)/get-room-members/route.ts
@@ -1,13 +1,12 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../(authentication)/auth/[...nextauth]/options";
-import { getPrismaClient } from "@/lib/prisma";
+import { getPrismaClient } from "@/lib/helpers/prisma";
 
 let prisma = getPrismaClient();
 
 export async function GET(req: NextRequest) {
   let session = await getServerSession(authOptions);
-  console.log("server session: ", session);
   if (!session || !session.user.roomName) {
     return NextResponse.json(
       { Error: "Unauthorized request" },
@@ -34,9 +33,9 @@ export async function GET(req: NextRequest) {
         },
       },
     });
+
     return NextResponse.json({ ...dbResult });
   } catch (error) {
-    console.log(error);
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }

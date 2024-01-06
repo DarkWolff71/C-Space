2385dd2fb6ea3b6a58cf484a6b0fc5f89f0a517c
Praveen <praveenpvg.71@gmail.com>
2024-01-07 00:49:51 +0530

    final leave room route

--- src/app/api/(rooms)/leave-room/route.ts
+++ src/app/api/(rooms)/leave-room/route.ts
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { authOptions } from "../../(authentication)/auth/[...nextauth]/options";
 import { leaveRoomRequestValidator } from "@/validators/roomsValidators";
-import { getPrismaClient } from "@/lib/prisma";
+import { getPrismaClient } from "@/lib/helpers/prisma";
 
 let prisma = getPrismaClient();
 
@@ -10,6 +10,7 @@ export async function PUT(req: NextRequest) {
   let requestBody = await req.json();
   let parsedRequestBody = leaveRoomRequestValidator.safeParse(requestBody);
   let session = await getServerSession(authOptions);
+
   if (!(parsedRequestBody.success && session && session.user.email)) {
     return NextResponse.json({ error: "Invalid request." }, { status: 400 });
   }

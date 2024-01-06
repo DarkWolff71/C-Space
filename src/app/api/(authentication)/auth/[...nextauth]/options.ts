a62b43cb0af6bd6ec29e50ac752180d107d59688
Praveen <praveenpvg.71@gmail.com>
2024-01-07 01:11:16 +0530

    final next auth options

--- src/app/api/(authentication)/auth/[...nextauth]/options.ts
+++ src/app/api/(authentication)/auth/[...nextauth]/options.ts
@@ -1,8 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { getPrismaClient } from "@/lib/helpers/prisma";
 
@@ -13,10 +11,6 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
   providers: [
-    // GitHubProvider({
-    //     clientId: process.env.GITHUB_ID as string,
-    //     clientSecret: process.env.GITHUB_SECRET as string,
-    // }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
@@ -27,44 +21,13 @@ export const authOptions: NextAuthOptions = {
         },
       },
     }),
-    // CredentialsProvider({
-    //   name: "Credentials",
-    //   credentials: {
-    //     username: {
-    //       label: "Username:",
-    //       type: "text",
-    //       placeholder: "your-cool-username",
-    //     },
-    //     password: {
-    //       label: "Password:",
-    //       type: "password",
-    //       placeholder: "your-awesome-password",
-    //     },
-    //   },
-    //   async authorize(credentials) {
-    //     // This is where you need to retrieve user data
-    //     // to verify with credentials
-    //     // Docs: https://next-auth.js.org/configuration/providers/credentials
-    //     const user = { id: "42", name: "Dave", password: "nextauth" };
-    //     console.log("hellooooooooo line 30");
-
-    //     // if (
-    //     //   credentials?.username === user.name &&
-    //     //   credentials?.password === user.password
-    //     // ) {
-    //     return user;
-    //     // } else {
-    //     //   return null;
-    //     // }
-    //   },
-    // }),
   ],
+
   //@ts-ignore
   adapter: PrismaAdapter(prisma),
   callbacks: {
-    async jwt({ token, user, session, trigger }) {
+    async jwt({ token, session, trigger }) {
       if (trigger === "update") {
-        console.log("line 677777777777");
         if (session.roomName && session.role) {
           if (session.role === "owner") {
             let dbResult = await prisma.room.findUnique({
@@ -82,20 +45,14 @@ export const authOptions: NextAuthOptions = {
                 },
               },
             });
-            console.log("line 80");
-
             // @ts-ignore
             if (dbResult && dbResult.owners.length > 0) {
-              console.log("line 84");
-
               token.roomName = session.roomName;
               token.role = "owner";
               // @ts-ignore
               token.ownersInCurrentRoom = dbResult._count.owners;
             }
           } else if (session.role === "editor") {
-            console.log("line 90");
-
             let dbResult = await prisma.room.findUnique({
               where: { name: session.roomName },
               select: {
@@ -113,8 +70,6 @@ export const authOptions: NextAuthOptions = {
             });
             //@ts-ignore
             if (dbResult && dbResult.editors.length > 0) {
-              console.log("line 105");
-
               token.roomName = session.roomName;
               token.role = "editor";
               //@ts-ignore
@@ -122,40 +77,15 @@ export const authOptions: NextAuthOptions = {
             }
           }
         } else {
-          // console.log("I'm in else update");
           const { roomName, role, ...newToken } = token;
           token = newToken;
         }
       }
-      // console.log("I'm in auth JWT callback");
-      // console.log("token: ", token);
-      // console.log();
-      // console.log("user: ", user);
-      // console.log("session: ", session);
-      // console.log();
-      // console.log("I'm exiting auth JWT callback");
 
       return token;
     },
-    // async jwt({ token, account, profile, user, session }) {
-    //   console.log("hellooooooooo");
-    //   // Persist the OAuth access_token and or the user id to the token right after signin
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //     // console.log("line 47: ");
-    //     // console.log("token: ", token);
-    //     // console.log("account: ", account);
-    //     // console.log("user: ", user);
-    //     // console.log("session: ", session);
-    //     // console.log("profile: ", profile);
-    //   }
-    //   return token;
-    // },
-    async session({ session, token, user }) {
-      // console.log("I'm in auth session callback");
-      // console.log("session: ", session);
-      // console.log("token: ", token);
-      // console.log("user: ", user);
+
+    async session({ session, token }) {
       if (token) {
         if (token.roomName && token.role && token.ownersInCurrentRoom) {
           // @ts-ignore
@@ -170,7 +100,6 @@ export const authOptions: NextAuthOptions = {
           delete session.user.ownersInCurrentRoom;
         }
       }
-      // console.log("I'm exiting auth session callback");
 
       return session;
     },

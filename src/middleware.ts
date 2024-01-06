359008433c31271ab9b2a2a95072844f37963353
Praveen <praveenpvg.71@gmail.com>
2024-01-06 19:45:36 +0530

    final middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
+
+const ROOMS_URLS = [
+  "/rooms",
+  "/api/get-rooms",
+  "/api/create-room",
+  "/api/delete-room",
+  "/api/leave-room",
+];
+
+const RECEIVED_REQUESTS_URLS = [
+  "/received-requests",
+  "/api/get-received-requests",
+  "/api/cancel-join-request",
+  "/api/accept-join-request",
+];
+
+export default withAuth(async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const path = req.nextUrl.pathname;
+
+  if (RECEIVED_REQUESTS_URLS.includes(path)) {
+    return NextResponse.next();
+  }
+  if (!token?.roomName) {
+    if (!ROOMS_URLS.includes(path)) {
+      return NextResponse.redirect(new URL("/rooms", req.url));
+    }
+  }
+  console.log("line 22 middleware");
+  return NextResponse.next();
+});

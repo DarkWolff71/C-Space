a54f69970e00de8b37e7b0cf7dd7ca4b921cecbf
Praveen <praveenpvg.71@gmail.com>
2024-01-06 23:29:51 +0530

    final full width bg component

--- src/components/ui/FullWidthBg.tsx
+++ src/components/ui/FullWidthBg.tsx
@@ -4,14 +4,22 @@ import { cn } from "../../lib/utils";
 type Props = {
   children?: React.ReactNode;
   className?: string;
+  enableHover?: boolean;
 };
 
-export function FullWidthBg({ children, className }: Props) {
+export function FullWidthBg({
+  children,
+  className,
+  enableHover = false,
+}: Props) {
   return (
     <>
       <div
         className={cn(
           "rounded bg-gray-50 dark:bg-gray-800 p-1 min-h-fit",
+          {
+            "hover:bg-gray-100 dark:hover:bg-gray-700": enableHover,
+          },
           className
         )}
       >

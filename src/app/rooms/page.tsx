0dfa52a3e76c7319cdfdc0ae826b4061d9523219
Praveen <praveenpvg.71@gmail.com>
2024-01-07 00:26:55 +0530

    rooms page

---
+++ src/app/rooms/page.tsx
@@ -0,0 +1,99 @@
+import { CreateRoomCard, PageContent, RoomCard } from "@/components/ui";
+import { Role } from "@/lib/constants/roles";
+import { GetRoomsResponse } from "@/types/response";
+import React from "react";
+import { getCookiesString } from "@/lib/helpers/cookiesString";
+import { authOptions } from "../api/(authentication)/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+
+async function getRoomsData() {
+  let response = await fetch("http://localhost:3000/api/get-rooms", {
+    headers: {
+      Cookie: getCookiesString(),
+    },
+  });
+  const responseBody = await response.json();
+
+  return responseBody;
+}
+
+export default async function RoomsPage() {
+  // TODO: Remove tempProps after testing
+  let tempProps = {
+    editors: 2,
+    owners: 5,
+    role: Role.EDITOR,
+    name: "Dark Wold",
+    requests: 45,
+    unpublishedVideos: 56,
+  };
+  let roomsData: GetRoomsResponse = await getRoomsData();
+  const session = await getServerSession(authOptions);
+  const roleInCurrentRoom =
+    session?.user.role === "editor" ? Role.EDITOR : Role.OWNER;
+  const ownersInCurrentRoom = session?.user.ownersInCurrentRoom ?? 0;
+
+  return (
+    <>
+      <PageContent title="Rooms">
+        <CreateRoomCard className="mb-4"></CreateRoomCard>
+        <div className="grid grid-cols-4 gap-4 ">
+          {roomsData?.ownerRooms &&
+            roomsData?.ownerRooms.map((room) => {
+              return (
+                <RoomCard
+                  key={room.name}
+                  {...{
+                    editors: room._count.editors,
+                    owners: room._count.owners,
+                    role: Role.OWNER,
+                    name: room.name,
+                    requests: room._count.joinRequests,
+                    unpublishedVideos: room._count.unpublishedVideos,
+                    ownersInCurrentRoom: ownersInCurrentRoom,
+                    roleInCurrentRoom: roleInCurrentRoom,
+                  }}
+                ></RoomCard>
+              );
+            })}
+          {roomsData?.editorRooms &&
+            roomsData?.editorRooms.map((room) => {
+              return (
+                <RoomCard
+                  key={room.name}
+                  {...{
+                    editors: room._count.editors,
+                    owners: room._count.owners,
+                    role: Role.EDITOR,
+                    name: room.name,
+                    requests: room._count.joinRequests,
+                    unpublishedVideos: room._count.unpublishedVideos,
+                    ownersInCurrentRoom: ownersInCurrentRoom,
+                    roleInCurrentRoom: roleInCurrentRoom,
+                  }}
+                ></RoomCard>
+              );
+            })}
+
+          {/* TODO: Remove this after testing 
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard>
+          <RoomCard {...tempProps} className="w-full"></RoomCard> */}
+        </div>
+      </PageContent>
+    </>
+  );
+}
